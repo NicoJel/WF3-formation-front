6c2354e36715805826094e9b92ef528b3f50ffa6
NicoJel <39770582+NicoJel@users.noreply.github.com>
2018-06-10 23:15:46 +0200

    exo 3 jquery

--- JQUERY/assets/js/08.contact.js
+++ JQUERY/assets/js/08.contact.js
@@ -1,90 +1,121 @@
 
+$(document).ready(function(){
+
+    var tableaucontact = []
 
 $('#contact').submit(function(event){
 
 //PREVENT
 event.preventDefault();
 
+//CLEAR
+$('.container .is-invalid').removeClass('is-invalid');
+$('.container .alert-danger').remove();
+console.log('remove');
+$('.container .alert-success').remove();
+
 // RECUP DES VARIABLES
 
-nom = $('#nom');
-prenom = $('#prenom');
-email = $('#email');
-tel = $('#tel');
-contacts = $('LesContacts'); // tableau contact
+var nom = $('#nom');
+var prenom = $('#prenom');
+var email = $('#email');
+var tel = $('#tel');
+var contacts = $('#LesContacts'); // tableau contact
 
 
 
 // MESSAGE D'ERREUR
 
-
-$('#form').validator().on('submit', function (e) {
-    if (e.isDefaultPrevented()) {
-      // handle the invalid form...
-    } else {
-      // everything looks good!
-    }
-  })
-
-
   
-if ( nom.val().length === 0) {
-    nom.addClass('is-invalid');
-    $(`
-        <div class="invalid-feedback">
-            vous devez saisir votre nom.
-        </div>
-    `).appendTo(nom.parent());
-};
+    if ( nom.val().length === 0) {
+        nom.addClass('is-invalid');
+        
+    };
 
-if ( prenom.val().length === 0) {
-    prenom.addClass('is-invalid');
-    $(`
-        <div class="invalid-feedback">
-            vous devez saisir votre prenom.
-        </div>
-    `).appendTo(prenom.parent());
-};
+    if ( prenom.val().length === 0) {
+        prenom.addClass('is-invalid');
+        
+    };
 
-email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-if(!email_regex.test(email.val())){
-    email.addClass('is-invalid');
-    $(`
-        <div class="invalid-feedback">
-            cet email n'est pas valide
-        </div>
-    `).appendTo(email.parent());  }
+    email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    if(!email_regex.test(email.val())){
+        email.addClass('is-invalid');
+     }
 
     var regExp = ("(0|\\+33|0033)[1-9][0-9]{8}")
     var phone = tel.val().match(regExp);
     if (!phone) {
         tel.addClass('is-invalid');
-    $(`
-        <div class="invalid-feedback">
-            ce tel n'est pas valide
-        </div>
-    `).appendTo(tel.parent());  }
-        
+    }
+      
+    var check = false;
     if( $(this).find('.is-invalid').length === 0 ) {
 
-        $(this).replaceWith(`
+        $(contacts).before(`
             <div class="alert alert-success" role="alert">
-                Votre contact à bien été transmis !
-                Nous vous répondrons dans les meilleurs délais.
+            <strong>Félicitation !</strong> Votre contact est ajouté.
             </div>
         `);
-
+        check = true;
         } else {
 
-        $(this).prepend(`
+        $(contacts).before(`
             <div class="alert alert-danger" role="alert">
-                Nous n'avons pas été en mesure de valider votre contact. 
-                Vérifiez vos informations.
-            </div>
+            <strong>Attention !</strong> Veuillez bien remplir tous les champs.
+            </div>          
         `);
+        
 
-        }
+    }
 
+    // CREATION DU TABLEAU
 
+if (check) {
+    tableaucontact.push(
+        contactn = [ 
+            nom.val(),                                      
+            prenom.val(), 
+            email.val(), 
+            tel.val() 
+        ]);
 
+    let le = tableaucontact.length;
+    var tbody = document.getElementsByClassName('monputaindetbody')[0];
+
+    console.log(tableaucontact);
+    console.log(contactn);
+        for (i=0; i<le; i++){
+            var tr = document.createElement("tr");
+            let f = i-1;
+            console.log(f);
+            for(j=0; j<tableaucontact[i].length; j++){
+                var td = document.createElement("td");
+                var txt = document.createTextNode(tableaucontact[i][j]);
+                
+                    if ((f > -1) && (tableaucontact[i][j] == tableaucontact[f][j]) ) {
+
+                        $(contacts).before(`
+                        <div class="alert alert-danger" role="alert">
+                        <strong>Attention ! </strong>` + tableaucontact[f][j] + ` est déjà utilisé
+                        </div>          
+                        `);
+                        $("tbody :last-child").remove()
+                        $('.container .alert-success').remove();
+                        
+                        break;
+                    }
+
+                td.appendChild(txt);
+                tr.appendChild(td);
+            };
+            tbody.appendChild(tr);
+        };
+
+        if(le > 0){
+            $('#LesContacts .aucuncontact').remove();
+        };
+    }
+      
+   
+});
 });
