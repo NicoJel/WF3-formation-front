d9d0c8acf03072db87dfec77b6b640eff08efd8c
NicoJel <39770582+NicoJel@users.noreply.github.com>
2018-06-11 17:05:16 +0200

    11/06/18
    ex3 4 5

--- JQUERY/assets/js/08.contact.js
+++ JQUERY/assets/js/08.contact.js
@@ -11,7 +11,6 @@ event.preventDefault();
 //CLEAR
 $('.container .is-invalid').removeClass('is-invalid');
 $('.container .alert-danger').remove();
-console.log('remove');
 $('.container .alert-success').remove();
 
 // RECUP DES VARIABLES
@@ -21,7 +20,7 @@ var prenom = $('#prenom');
 var email = $('#email');
 var tel = $('#tel');
 var contacts = $('#LesContacts'); // tableau contact
-
+let infovalides = true;
 
 
 // MESSAGE D'ERREUR
@@ -32,22 +31,38 @@ var contacts = $('#LesContacts'); // tableau contact
         
     };
 
+    /*if ( nom.val().length === 0) {
+        infovalides = false;
+    };*/
+
     if ( prenom.val().length === 0) {
         prenom.addClass('is-invalid');
         
     };
+
+    /*if ( prenom.val().length === 0) {
+        infovalides = false;
+    };*/
 
     email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
     if(!email_regex.test(email.val())){
         email.addClass('is-invalid');
      }
 
+    /*if (!email_regex.test(email.val())){
+        infovalides = false;
+    };*/
+
     var regExp = ("(0|\\+33|0033)[1-9][0-9]{8}")
     var phone = tel.val().match(regExp);
     if (!phone) {
         tel.addClass('is-invalid');
     }
-      
+
+    /*if (!phone) {
+        infovalides = false;
+    };*/
+
     var check = false;
     if( $(this).find('.is-invalid').length === 0 ) {
 
@@ -82,15 +97,15 @@ if (check) {
     let le = tableaucontact.length;
     var tbody = document.getElementsByClassName('monputaindetbody')[0];
 
-    console.log(tableaucontact);
-    console.log(contactn);
-        for (i=0; i<le; i++){
+    console.log("tableau avant boucle " + tableaucontact);
+    console.log("contact avant boucle " + contactn);
+    let i = le-1;
+        for (i; i<le; i++){
             var tr = document.createElement("tr");
             let f = i-1;
-            console.log(f);
+            console.log("i-1= " + f);
             for(j=0; j<tableaucontact[i].length; j++){
-                var td = document.createElement("td");
-                var txt = document.createTextNode(tableaucontact[i][j]);
+                
                 
                     if ((f > -1) && (tableaucontact[i][j] == tableaucontact[f][j]) ) {
 
@@ -99,11 +114,19 @@ if (check) {
                         <strong>Attention ! </strong>` + tableaucontact[f][j] + ` est déjà utilisé
                         </div>          
                         `);
-                        $("tbody :last-child").remove()
+                        //$('.monputaindetbody tr:last').remove();
+                        //$('tr td:last').remove();
                         $('.container .alert-success').remove();
+                        tableaucontact.splice(-1,1);
+                        contactn = 0;
+                        console.log("tableau dans erreur " + tableaucontact);
+                        console.log("contact dans erreur " + contactn);
+
                         
                         break;
                     }
+                var td = document.createElement("td");
+                var txt = document.createTextNode(tableaucontact[i][j]);
 
                 td.appendChild(txt);
                 tr.appendChild(td);
@@ -114,6 +137,7 @@ if (check) {
         if(le > 0){
             $('#LesContacts .aucuncontact').remove();
         };
+        $('#contactn')[0].reset();
     }
       
    
--- JQUERY/assets/js/soluce.js
+++ JQUERY/assets/js/soluce.js
@@ -9,7 +9,35 @@ $(function() {
     -------------------------------------------------------------- */
 
     // -- Fonction ajouterContact(Contact) : Ajouter un Contact dans le tableau de Contacts, mettre à jour le tableau HTML, réinitialiser le formulaire et afficher une notification.
-    function ajouterContact(UnContact) {}
+    function ajouterContact(UnContact) {
+
+        // Ajouter un Contact dans le tableau de Contacts
+
+        CollectionDeContacts.push(contact);
+
+        // mettre à jour le tableau HTML
+
+        $('.aucuncontact').hide();
+        $(`
+            <tr>
+                <td>${Contact.nom}</td>
+                <td>${Contact.prenom}</td>
+                <td>${Contact.email}</td>
+                <td>${Contact.tel}</td>
+            </tr>
+        `)
+        // réinitialiser le formulaire
+
+        $('#contact')[0].reset();
+        $('#contact').get(0).reset();
+        $('#contact').trigger('reset')
+        $('#contact .form-control').val('');
+        document.getElementById('contact').reset();
+
+        // afficher une notification
+
+        $('.alert-contact').fadein().delay(4000).fadeOut();
+    }
 
     // -- Fonction RéinitialisationDuFormulaire() : Après l'ajout d'un contact, on remet le formulaire à 0 !
     function reinitialisationDuFormulaire() {}
@@ -29,43 +57,88 @@ $(function() {
     -------------------------------------------------------------- */
 
     // -- Détection de la soumission de mon Formulaire
-    $('#contact').on('submit', function(e) {
-
-        // -- Voir le contenu de l'évènement
-
-        // -- Stopper la redirection de la page
-
-        // -- Récupération des champs à vérifier
-
-        // -- Vérification des informations...
         
-        // -- Vérification du Prénom
+$('#contact').submit(function(event){
 
-        // -- Vérification du Nom
+    //PREVENT
+    event.preventDefault();
+    
+    //CLEAR
+    $('.container .is-invalid').removeClass('is-invalid');
+    $('.container .alert-danger').remove();
+    $('.container .alert-success').remove();
+    
+    // RECUP DES VARIABLES
+    
+    var nom = $('#nom');
+    var prenom = $('#prenom');
+    var email = $('#email');
+    var tel = $('#tel');
+    var contacts = $('#LesContacts'); // tableau contact
+    let infovalides = true;
+    
+    
+    // MESSAGE D'ERREUR
+    
+     
+    
+        if ( nom.val().length === 0) {
+            infovalides = false;
+        };
+    
+    
+        if ( prenom.val().length === 0) {
+            infovalides = false;
+        };
 
-        // -- Vérification du Tel
-
-        // -- Vérification du Mail
-
-        if(...) {
+        email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+        if (!email_regex.test(email.val())){
+            infovalides = false;
+        };
+    
+        var regExp = ("(0|\\+33|0033)[1-9][0-9]{8}")
+        var phone = tel.val().match(regExp);
+        if (!phone) {
+            infovalides = false;
+        };
+    
+    
+        // CREATION DU TABLEAU
+    
+    if (infovalides) {
+        let contact ={
+            nom : nom.val(),                                      
+            prenom : prenom.val(), 
+            emil : email.val(), 
+            tel : tel.val() 
+        }
+       
             
-            // -- Tous est correct, Préparation du Contact
+    function contactpresent(email){
+        let estpresent = false;
 
-            // -- Observons dans la console
-            console.log(Contact);
+        for (i=0; i<CollectionDeContacts.length; i++){
 
-            // -- Vérification avec EstCeQunContactEstPresent
-            if(!estCeQunContactEstPresent(...) {
+            if(UnEmail === CollectionDeContacts[i].email){
+                estpresent = true;
+                break;
+            }
+            return estpresent;
+        }
+    }
+            if(!estCeQunContactEstPresent(estpresent)) {
                 
                 // -- Ajout du Contact
                 ajouterContact(Contact);
 
             } else {                    
                 // -- Alert si contact présent
+                alert('attention contact dja present')
             }
 
         } else {
             // -- Alert si erreur dans l'un des champs
+            alert('veuillez remplir ts les champs')
         }       
 
     });
